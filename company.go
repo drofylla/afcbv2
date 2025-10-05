50782f3ba61ae24151bd62b80c31611c307fee7a
drofylla <drofylla@gmail.com>
2025-10-05 13:54:10 +0800

    add company

---
+++ company.go
@@ -0,0 +1,12 @@
+package main
+
+type Company struct {
+	ID                       string
+	Name                     string
+	BankName                 string
+	AccountNumber            string
+	AccountDocumentPath      string
+	RegistrationNumber       string
+	RegistrationDocumentPath string
+	CreatedAt                string
+}
