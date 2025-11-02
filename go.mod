d105b4d515b159e932baee45a5dbb3b8be45ef0a
drofylla <drofylla@gmail.com>
2025-11-02 15:47:12 +0800

    add gen cc pdf

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require github.com/matoous/go-nanoid v1.5.1
 
 require (
 	github.com/gorilla/mux v1.8.1
+	github.com/jung-kurt/gofpdf v1.16.2
 	github.com/mattn/go-sqlite3 v1.14.32
 )
--- pdf_service.go
+++ pdf_service.go
@@ -1,7 +1,81 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
 type PDFService struct{}
 
 func NewPDFService() *PDFService {
 	return &PDFService{}
+}
+
+// Generate PDF for contact card
+func (p *PDFService) GenerateContactCardPDF(contact *Contact, companyName string) ([]byte, error) {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	// PDF setup
+	pdf.SetHeaderFunc(func() {
+		//Header
+		pdf.SetFont("Arial", "B", 16)
+		pdf.Cell(40, 10, "AFcb Contact Card")
+		pdf.Ln(12)
+	})
+
+	pdf.SetFooterFunc(func() {
+		//Footer
+		pdf.SetY(-15)
+		pdf.SetFont("Arial", "I", 8)
+		pdf.Cell(0, 10, fmt.Sprintf("Generated on %s", time.Now().Format("January 2, 2006 at 15:04")))
+	})
+
+	pdf.AddPage()
+
+	//Title
+	pdf.SetFont("Arial", "B", 20)
+	pdf.Cell(0, 15, "Contact Information")
+	pdf.Ln(20)
+
+	//Contact details
+	//Name
+	pdf.SetXY(45, 40)
+	pdf.SetFont("Arial", "B", 14)
+	pdf.Cell(0, 8, fmt.Sprintf("%s, %s", contact.FirstName, contact.LastName))
+	pdf.Ln(10)
+	//IDF
+	pdf.SetX(45)
+	pdf.SetFont("Arial", "", 12)
+	pdf.Cell(0, 7, fmt.Sprintf("ID: %s", contact.ID))
+	pdf.Ln(7)
+	//Type
+	pdf.SetX(45)
+	pdf.Cell(0, 7, fmt.Sprintf("Type: %s", contact.ContactType))
+	//Company
+	if companyName != "" {
+		pdf.SetX(45)
+		pdf.Cell(0, 7, fmt.Sprintf("Company: %s", companyName))
+		pdf.Ln(7)
+	}
+	pdf.Ln(15)
+	//Contact Methods
+	pdf.SetFont("Arial", "B", 14)
+	pdf.Cell(0, 10, "Contact Methods")
+	pdf.Ln(12)
+	//Email
+	pdf.SetFont("Arial", "B", 11)
+	pdf.Cell(25, 7, "Email:")
+	pdf.SetFont("Arial", "", 11)
+	pdf.Cell(0, 7, contact.Email)
+	pdf.Ln(8)
+	//Phone
+	pdf.SetFont("Arial", "B", 11)
+	pdf.Cell(25, 7, "Phone:")
+	pdf.SetFont("Arial", "", 11)
+	pdf.Cell(0, 7, contact.Phone)
+	pdf.Ln(8)
+
+	return pdf.OutputBytes(), nil
 }
